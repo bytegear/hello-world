bc5dc9ae1e96411d5c8e9985e7dea02e873ebac2
bytegear <yoonie@hotmail.de>
2017-10-05 14:17:26 +0200

    Adding the Main and Point class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by bytegear on 05.10.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/Point.java
@@ -0,0 +1,14 @@
+/**
+ * Created by bytegear on 05.10.17.
+ */
+public class Point {
+
+    private float x,y,z;
+
+    public Point(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
